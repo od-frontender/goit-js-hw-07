dc82f64fc606fa41539ebcc43ab7b85f87a53b52
od-frontender <od.frontender@gmail.com>
2021-06-30 15:45:11 +0300

    доработал 5 и 7 задание

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,12 @@
 const inputEl = document.querySelector('#name-input');
 const textEl = document.querySelector('#name-output');
+console.log(textEl.textContent);
 
-const onInputChange = event => {
-  textEl.textContent = event.currentTarget.value;
+const onInputChange = () => {
+  if (inputEl.value === '') {
+    textEl.innerHTML = 'незнакомец';
+  } else {
+    textEl.innerHTML = inputEl.value;
+  }
 };
 inputEl.addEventListener('input', onInputChange);
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
+textEl.style.fontSize = inputEl.value + 'px';
 
 const changeFontSize = () => {
   textEl.style.fontSize = inputEl.value + 'px';
 };
-inputEl.addEventListener('change', changeFontSize);
+inputEl.addEventListener('input', changeFontSize);
